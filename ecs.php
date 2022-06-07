f96337937376942b798bafd44b95b6ffb2b37261
Benedict Massolle <bm@presentprogressive.de>
2022-06-07 15:12:53 +0200

    minor fixes

--- ecs.php
+++ ecs.php
@@ -6,7 +6,7 @@ use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->sets([__DIR__.'/vendor/contao/easy-coding-standard/config/contao.php']);
+    $ecsConfig->sets([__DIR__.'/tools/ecs/vendor/contao/easy-coding-standard/config/contao.php']);
 
     $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
         'header' => "This file is part of [package name].\n\n(c) John Doe\n\n@license LGPL-3.0-or-later"
