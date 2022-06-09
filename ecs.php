92f8f5d094c568b4d55754ac5c3851d4132cede9
Benedict Massolle <bm@presentprogressive.de>
2022-06-09 12:48:02 +0200

    Use PHP_VERSION_ID instead

--- ecs.php
+++ ecs.php
@@ -12,7 +12,7 @@ return static function (ECSConfig $ecsConfig): void {
         'header' => "This file is part of [package name].\n\n(c) John Doe\n\n@license LGPL-3.0-or-later"
     ]);
 
-    if (version_compare(PHP_VERSION, '8.0.0', '<')) {
+    if (PHP_VERSION_ID < 80000) {
         $ecsConfig->ruleWithConfiguration(\PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer::class, ['elements' => ['arrays'], 'after_heredoc' => true]);
     }
 
