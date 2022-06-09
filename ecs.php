c3be58a262569266cab5dfb0d339d19baeda0e4e
bezin <bm@presentprogressive.de>
2022-06-09 18:13:23 +0200

    Add PHPstan and move code quality tools to subfolder (see #11)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    a7e81f39 Add PHPstan
    5e01b4e6 Move ecs and phpstan to tools subfolder
    f9633793 minor fixes
    0a54decb Add CI workflows for ECS and PHPstan
    b58236ea Normalize the composer.json file
    09a11ecc Include composer.lock in tools/*
    51bc1b9f Do not schedule running workflows
    4baaa84b Merge branch 'add-phpstan' of github.com:bezin/skeleton-bundle into a…
    4c78dc52 add unit test runner in GitHub workflow
    3f150009 Run ECS with the lowest supported PHP version
    efe7b062 Fix code style in workflow.yml
    a0be1c1e Remove if conditions in workflow file
    ce8b4b8a Normalize the composer.json file
    79b8d517 Update workflow file
    4a2cc18d Merge branch 'add-phpstan' of github.com:bezin/skeleton-bundle into a…
    ff61a038 Revert "Update workflow file"
    3077561e Use alternate configuration if using PHP 7
    92f8f5d0 Use PHP_VERSION_ID instead
    158f435c Skip PhpUnitExpectationFixer
    99896813 Update ecs dependencies
    cb0af3ae Fix typo
    
    Co-authored-by: Leo Feyer <github@contao.org>
    Co-authored-by: David Greminger <bytehead@users.noreply.github.com>

--- ecs.php
+++ ecs.php
@@ -6,11 +6,16 @@ use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->sets([__DIR__.'/vendor/contao/easy-coding-standard/config/contao.php']);
+    $ecsConfig->sets([__DIR__.'/tools/ecs/vendor/contao/easy-coding-standard/config/contao.php']);
 
     $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
         'header' => "This file is part of [package name].\n\n(c) John Doe\n\n@license LGPL-3.0-or-later"
     ]);
+
+    if (PHP_VERSION_ID < 80000) {
+        $ecsConfig->ruleWithConfiguration(\PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer::class, ['elements' => ['arrays'], 'after_heredoc' => true]);
+        $ecsConfig->skip([\PhpCsFixer\Fixer\PhpUnit\PhpUnitExpectationFixer::class]); // see https://github.com/symplify/symplify/issues/3130
+    }
 
     // Adjust the configuration according to your needs.
 };
