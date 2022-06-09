cb0af3ae15aca14051c5f9c2035d115b6199f9a9
bezin <bm@presentprogressive.de>
2022-06-09 13:52:12 +0200

    Fix typo
    Co-authored-by: David Greminger <bytehead@users.noreply.github.com>

--- ecs.php
+++ ecs.php
@@ -14,7 +14,7 @@ return static function (ECSConfig $ecsConfig): void {
 
     if (PHP_VERSION_ID < 80000) {
         $ecsConfig->ruleWithConfiguration(\PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer::class, ['elements' => ['arrays'], 'after_heredoc' => true]);
-        // SKip PhpUnitExpecationFixer (see https://github.com/symplify/symplify/issues/3130)
+        // Skip PhpUnitExpecationFixer (see https://github.com/symplify/symplify/issues/3130)
         $ecsConfig->skip([\PhpCsFixer\Fixer\PhpUnit\PhpUnitExpectationFixer::class]);
     }
 
