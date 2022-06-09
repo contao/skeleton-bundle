3077561e8b3ffed2ef5d39b5d7929d9d1cbf8d04
Benedict Massolle <bm@presentprogressive.de>
2022-06-09 12:41:04 +0200

    Use alternate configuration if using PHP 7

--- ecs.php
+++ ecs.php
@@ -12,5 +12,9 @@ return static function (ECSConfig $ecsConfig): void {
         'header' => "This file is part of [package name].\n\n(c) John Doe\n\n@license LGPL-3.0-or-later"
     ]);
 
+    if (version_compare(PHP_VERSION, '8.0.0', '<')) {
+        $ecsConfig->ruleWithConfiguration(\PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer::class, ['elements' => ['arrays'], 'after_heredoc' => true]);
+    }
+
     // Adjust the configuration according to your needs.
 };
